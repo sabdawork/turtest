f507aed60fdacd4d298954937b757141409ea2f5
sabdahtb <sabdahutabarat2804@gmail.com>
2024-01-31 15:03:57 +0800

    update bash

--- apps/web/bash-script.sh
+++ apps/web/bash-script.sh
@@ -4,7 +4,7 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
     echo "🛑 - Build cancelled"
-  exit 0;
+  exit 1;
 
 else
   echo "✅ - Build can proceed"
